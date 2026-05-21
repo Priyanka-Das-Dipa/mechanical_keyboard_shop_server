9f0c897ffb05f18a9fee8cec29d6c5283c2270e4
Priyanka-Das-Dipa <priyankadipa9102@gmail.com>
2026-05-21 06:29:34 +0600

    Fix create product schema

--- src/product/dto/create-product.dto.ts
+++ src/product/dto/create-product.dto.ts
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-unsafe-return */
+import { Transform, Type } from 'class-transformer';
 import { IsString, IsNumber, IsArray, IsOptional } from 'class-validator';
 
 export class CreateProductDto {
@@ -10,21 +12,31 @@ export class CreateProductDto {
   @IsString()
   description!: string;
 
+  @Type(() => Number)
   @IsNumber()
   price!: number;
 
   @IsString()
   currency!: string;
 
+  @Type(() => Number)
   @IsNumber()
   rating!: number;
 
+  @Type(() => Number)
   @IsNumber()
   reviewCount!: number;
 
+  @Type(() => Number)
   @IsNumber()
   quantity!: number;
 
+  @Transform(({ value }) => {
+    if (typeof value === 'string') {
+      return JSON.parse(value);
+    }
+    return value;
+  })
   @IsArray()
   @IsOptional()
   features!: string[];
